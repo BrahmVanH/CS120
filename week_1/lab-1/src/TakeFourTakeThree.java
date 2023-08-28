4802abeefcf632809fdac2b0cb1e7398718d7cc2
Brahm V <brahmvanh@gmail.com>
2023-08-28 09:33:10 -0400

    Commented out code. Added check to make sure player isnt removing more stones than are remaining in the pot

--- week_1/lab-1/src/TakeFourTakeThree.java
+++ week_1/lab-1/src/TakeFourTakeThree.java
@@ -1,21 +1,14 @@
+// Name: Brahm Van Houzen
+// Sources: I read about java while and do.. while on geeksforgeeks, but the code is all original
+
 public class TakeFourTakeThree {
     public static void main(String[] args) {
-        // Instruct user this is game take four
-        // Explain instructions
 
-        // Declare variable of numberOfStones
-        // Each user will take one to four stones from pot
-        // User to take last stone from pot wins
-        // Pot cannot be less than zero stones
-        // When pot is less than four stones, user can only take
-        // a maximum amount of stones equal to the number of stones left
-        // While number of stones is > 3, act normal
-        // While number of stones is < 4, stonesRemoved cannot be greater than remainingStones
-
-        // Declare variables for total stones in pot and amount of stones removed
+        // Declare variables for stone handling
         int stonesRemaining;
         int stonesRemoved;
 
+        // Explain instructions for players
         ConsoleIO.printLine("This is the game Take Four. It's a two player game. Players will choose the number ");
         ConsoleIO.printLine("of stones to start the game with and each player will choose to take 1 to 4 stones ");
         ConsoleIO.printLine("from the pot per turn. The player to take the last stone loses.");
@@ -23,50 +16,68 @@ public class TakeFourTakeThree {
 
         // Prompt users for how many stones they would like to begin the game with
         ConsoleIO.print("How many stones would you like to begin with? ");
+
+        // Save console input as stones remaining
         stonesRemaining = ConsoleIO.readInt();
 
-
+        // Game will proceed only if there are more than zero stones in the pot
         while (stonesRemaining > 0) {
+            // Change syntax of stones remaining update depending on number of stones in pot
+            if (stonesRemaining > 1) {
+                ConsoleIO.printLine("There are " + stonesRemaining + " stones left in the pot");
+            } else {
+                ConsoleIO.printLine("There is " + stonesRemaining + " stone left in the pot");
+            }
 
-            ConsoleIO.printLine("There are " + stonesRemaining + " stones left in the pot");
-
+            // Prompt player one for their turn
             ConsoleIO.print("Player 1, how many stones would you like to remove from the pot? ");
+            // Save console input as stones removed by user
             stonesRemoved = ConsoleIO.readInt();
-//            do {
-//                ConsoleIO.printLine("Please choose a number between 1 and 4");
-//                stonesRemoved = ConsoleIO.readInt();
-//
-//            } while (stonesRemoved > 4 || stonesRemoved < 1);
+
+            // Ensure only the appropriate number of stones are removed from the pot
             while (stonesRemoved > 4 || stonesRemoved < 1) {
                 ConsoleIO.printLine("Please choose a number between 1 and 4");
                 stonesRemoved = ConsoleIO.readInt();
             }
 
-            stonesRemaining -= stonesRemoved;
+            // Ensure user isn't taking more stones than are remaining in the pot
+            while (stonesRemoved > stonesRemaining) {
+                ConsoleIO.printLine("There aren't that many stones left in the pot, please choose an ");
+                ConsoleIO.printLine("appropriate number.");
+                stonesRemoved = ConsoleIO.readInt();
+            }
 
+            stonesRemaining -= stonesRemoved;
+            // Check to see if user took last stone
             if (stonesRemaining <= 0) {
                 ConsoleIO.printLine("There are 0 stones left in the pot, player 2 wins");
                 break;
             }
 
-            ConsoleIO.printLine("There are " + stonesRemaining + " stones left in the pot");
+            if (stonesRemaining > 1) {
+                ConsoleIO.printLine("There are " + stonesRemaining + " stones left in the pot");
+            } else {
+                ConsoleIO.printLine("There is " + stonesRemaining + " stone left in the pot");
+            }
 
             ConsoleIO.print("Player 2, how many stones would you like to remove from the pot? ");
             stonesRemoved = ConsoleIO.readInt();
 
-//            do {
-//                ConsoleIO.printLine("Please choose a number between 1 and 4");
-//                stonesRemoved = ConsoleIO.readInt();
-//
-//            } while (stonesRemoved > 4 || stonesRemoved < 1);
+
 
             while (stonesRemoved > 4 || stonesRemoved < 1) {
                 ConsoleIO.printLine("Please choose a number between 1 and 4");
                 stonesRemoved = ConsoleIO.readInt();
             }
 
+            while (stonesRemoved > stonesRemaining) {
+                ConsoleIO.printLine("There aren't that many stones left in the pot, please choose an ");
+                ConsoleIO.printLine("appropriate number.");
+                stonesRemoved = ConsoleIO.readInt();
+            }
 
             stonesRemaining -= stonesRemoved;
+
             if (stonesRemaining <= 0) {
                 ConsoleIO.printLine("There are 0 stones left in the pot, player 1 wins");
                 break;
