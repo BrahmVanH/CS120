5449d2fe51c3207485a0bae98e3616ca45d38747
Brahm V <brahmvanh@gmail.com>
2023-08-29 20:49:14 -0400

    I'm still not privy to how intellij sets up its projects. Im thinking Ill have to just keep a running /src with all of the games we make in class. Made a dice roll game with either 4-sided die or 6-sided die

---
+++ week_2/labTwo/src/DiceRollGame.java
@@ -0,0 +1,15 @@
+//Name: Brahm Van Houzen
+//References: Java Docs for Math.Random() and Math.Floor();
+
+public class DiceRollGame {
+    public static void main(String[] args) {
+        //Get a random number
+        int x;
+        x = Math.floor(Math.Random());
+        ConsoleIO.printLine("Is this thing on?")
+        //Use the random number to decide the outcome of a die roll
+        //Get a second random number
+        //Decide the outcome of a second die roll
+        //Print out which roll was higher
+    }
+}
