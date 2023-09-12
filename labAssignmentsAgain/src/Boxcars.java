fb49493e5edb55b346da04c63b2be1b55c60c129
Brahm V <brahmvanh@gmail.com>
2023-09-12 10:23:25 -0400

    Finsished lab 7 - box cars. Accidentally deleted my default jdk last night. Shit

--- labAssignmentsAgain/src/Boxcars.java
+++ labAssignmentsAgain/src/Boxcars.java
@@ -1,3 +1,9 @@
+// Name: Brahm Van Houzen
+// Sources: Assignment guide; Homework 3 DecimalFormat information
+
+import java.text.*;
+import java.math.*;
+
 public class Boxcars {
     public static void main(String[] args) {
         // Keep rolling two dice until we get boxcars (12)
@@ -22,6 +28,16 @@ public class Boxcars {
 
         ConsoleIO.printLine("Number of tries to get Boxcar: " + numberOfRolls);
 
+        DecimalFormat formatDecimal = new DecimalFormat("0.00");
+
+        // Set the rounding mode of the formatDecimal instance of DecimalFormat to HALF_UP
+        // If >= .5 round up, if < .5 round down
+        formatDecimal.setRoundingMode(RoundingMode.HALF_UP);
+
+        double probability = (100.0/36.0)* (Math.pow((35.0/36.0), (numberOfRolls - 1)));
+        String roundedProbability;
+        roundedProbability = formatDecimal.format(probability);
+        ConsoleIO.printLine("The probablilty of rolling the dice this many times to get boxcars is: " + roundedProbability + "%.");
     }
 
 
