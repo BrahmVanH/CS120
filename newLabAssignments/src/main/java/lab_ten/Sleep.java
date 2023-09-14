a09dc9ad73c13cde31c502295003ad6acbe0b644
Brahm V <brahmvanh@gmail.com>
2023-09-14 10:49:50 -0400

    Started SleepCalc. Lab 10

---
+++ newLabAssignments/src/main/java/lab_ten/Sleep.java
@@ -0,0 +1,14 @@
+package lab_ten;
+
+public class Sleep {
+  // Each hour of sleep lost = drop one IQ point
+  // Each subsequent lost hour = drop two IQ points each hour
+
+  // static method iqLoss, single parameter hoursOfSleepMissed, return number of Iq points dropped
+  public static int iqLoss(int hoursOfSleepMissed) {
+    return iqPointDrop
+  }
+
+  // static method that request number of hours slept 0-24, parameter totalHoursOfSleep, return hoursOfSleepMissed
+
+}
---
+++ newLabAssignments/src/main/java/lab_ten/SleepCalc.java
@@ -0,0 +1,9 @@
+
+public class SleepCalc {
+  public static void main(String[] args) {
+
+    // Call Sleep.iqDrop three times (three nights worth of sleep loss)
+
+    // Return total amount of iqLoss for all three nights
+  }
+}
