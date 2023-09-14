23959a4bc0711b5824a1b64be9a0154bd5e498b7
Brahm V <brahmvanh@gmail.com>
2023-09-14 10:40:03 -0400

    Made simple self-checkout program. KowMart

--- newLabAssignments/src/main/java/KowMart.java
+++ newLabAssignments/src/main/java/KowMart.java
@@ -1,8 +1,13 @@
 // Name: Brahm Van Houzen
 // Sources: Na
 
+import java.math.*;
+import java.text.*;
+
 public class KowMart {
   public static void main(String[] args) {
+
+    DecimalFormat decimalFormat = new DecimalFormat("0.00");
     // Print greeting.
 
     ConsoleIO.printLine("Welcome to KowMart!");
@@ -11,14 +16,14 @@ public class KowMart {
     ConsoleIO.printLine("How many items do you have to checkout? (1-10)");
     int numberOfItems;
     numberOfItems = ConsoleIO.readInt();
-    
+
     // As long as they gave a number too small or too big, loop and ask again
-    
+
     while (numberOfItems < 1 || numberOfItems > 10) {
       ConsoleIO.printLine("Please choose 1-10.");
       numberOfItems = ConsoleIO.readInt();
     }
-    
+
     // Add up the cost of each item
 
     int itemsCounter = 0;
@@ -39,15 +44,16 @@ public class KowMart {
       totalCost += itemCost;
     }
 
-    if (itemsCounter == numberOfItems) {
+    if (itemsCounter == numberOfItems - 1) {
       ConsoleIO.printLine("Enter the cost of the last item.");
       itemCost = ConsoleIO.readDouble();
       totalCost += itemCost;
-      
+
     }
 
     // Ask if a bag is needed
-    ConsoleIO.printLine("Would you like to purchase a bag for your items? Enter 'yes' or 'no'. ($0.05 for orders less than 3 items)");
+    ConsoleIO.printLine(
+        "Would you like to purchase a bag for your items? Enter 'yes' or 'no'. ($0.05 for orders less than 3 items)");
     boolean wouldLikeBag = false;
     String bagResponse;
     bagResponse = ConsoleIO.readLine();
@@ -65,9 +71,15 @@ public class KowMart {
       totalCost += 0.05;
     }
 
-    totalCost += (totalCost * )
+    String formatTotalCost = decimalFormat.format(totalCost);
+
+    ConsoleIO.printLine("Sub-total: $" + formatTotalCost);
+    ConsoleIO.printLine("Tax (6%): $" + decimalFormat.format(totalCost * 0.06));
+    // Add sales tax (MI sales tax)
+    totalCost += (totalCost * 0.06);
+    formatTotalCost = decimalFormat.format(totalCost);
 
     // print out the total.
-    ConsoleIO.printLine("The total for your order is $" + totalCost + "." );
+    ConsoleIO.printLine("Total: $" + formatTotalCost + ".");
   }
 }
